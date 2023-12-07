52d52fe10974b8af2c533a301d044fb7caf09836
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2023-12-07 18:49:57 +0200

    Add coordinates class

---
+++ AdventOfCode2023/Utils/Graph/Coordinates.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2023.Utils.Graph
+{
+    public class Coordinates(int x, int y)
+    {
+        public readonly int X = x;
+        public readonly int Y = y;
+
+        public Coordinates Move(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.North => new Coordinates(X, Y - 1),
+                Direction.South => new Coordinates(X, Y + 1),
+                Direction.East => new Coordinates(X + 1, Y),
+                Direction.West => new Coordinates(X - 1, Y),
+                Direction.NorthEast => new Coordinates(X + 1, Y - 1),
+                Direction.NorthWest => new Coordinates(X - 1, Y - 1),
+                Direction.SouthEast => new Coordinates(X + 1, Y + 1),
+                Direction.SouthWest => new Coordinates(X - 1, Y + 1),
+                _ => throw new Exception("Invalid direction"),
+            };
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is Coordinates other)
+            {
+                return X == other.X && Y == other.Y;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+    }
+
+    public enum Direction
+    {
+        North, South, East, West,
+        NorthEast, NorthWest, SouthEast, SouthWest
+    }
+}
---
+++ AdventOfCode2023Tests/Utils/CoordinatesTests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2023.Utils.Graph;
+using NUnit.Framework;
+
+namespace AdventOfCode2023.Utils.Tests
+{
+    [TestFixture()]
+    public class CoordinatesTests
+    {
+        [Test()]
+        [TestCase(0, 0, 0, 0, true, TestName = "EqualsTest where equals")]
+        [TestCase(0, 0, 0, 1, false, TestName = "EqualsTest where different")]
+        public void EqualsTest(int x1, int y1, int x2, int y2, bool expectedEquals)
+        {
+            var n1 = new Coordinates(x1, y1);
+            var n2 = new Coordinates(x2, y2);
+
+            var equals = n1.Equals(n2);
+            Assert.That(equals, Is.EqualTo(expectedEquals));
+        }
+    }
+}
