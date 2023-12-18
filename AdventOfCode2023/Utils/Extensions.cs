7ff8b8a727bb50649f2990058e4da8d12d1ae5f5
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2023-12-19 00:19:49 +0200

    Add topological sort for acyclic directed graphs

--- AdventOfCode2023/Utils/Extensions.cs
+++ AdventOfCode2023/Utils/Extensions.cs
@@ -133,5 +133,124 @@
 
             return (Math.Abs(solids - dashes) + border) / 2 + 1;
         }
+
+        /// <summary>
+        /// Topological Sorting (Kahn's algorithm) 
+        /// </summary>
+        /// <remarks>https://en.wikipedia.org/wiki/Topological_sorting</remarks>
+        /// <param name="graph">Directed acyclic graph</param>
+        /// <returns>Sorted nodes in topological order.</returns>
+        public static List<GraphNode> TopologicalSort(this IWeightedGraph graph)
+        {
+            HashSet<GraphNode> nodes = [];
+            foreach (var node in graph.Nodes())
+                nodes.Add(node);
+
+            HashSet<(GraphNode, GraphNode)> edges = [];
+            foreach (var connection in graph.Connections())
+                edges.Add((connection.Item1, connection.Item2));
+
+            // Empty list that will contain the sorted elements
+            var L = new List<GraphNode>();
+
+            // Set of all nodes with no incoming edges
+            var S = new HashSet<GraphNode>(nodes.Where(n => edges.All(e => e.Item2.Equals(n) == false)));
+
+            // while S is non-empty do
+            while (S.Count != 0)
+            {
+                //  remove a node n from S
+                var n = S.First();
+                S.Remove(n);
+
+                // add n to tail of L
+                L.Add(n);
+
+                // for each node m with an edge e from n to m do
+                foreach (var e in edges.Where(e => e.Item1.Equals(n)).ToList())
+                {
+                    var m = e.Item2;
+
+                    // remove edge e from the graph
+                    edges.Remove(e);
+
+                    // if m has no other incoming edges then
+                    if (edges.All(me => me.Item2.Equals(m) == false))
+                    {
+                        // insert m into S
+                        S.Add(m);
+                    }
+                }
+            }
+
+            // if graph has edges then
+            if (edges.Count != 0)
+            {
+                // return error (graph has at least one cycle)
+                return [];
+            }
+            else
+            {
+                // return L (a topologically sorted order)
+                return L;
+            }
+        }
+
+        /// <summary>
+        /// Topological Sorting (Kahn's algorithm) 
+        /// </summary>
+        /// <remarks>https://en.wikipedia.org/wiki/Topological_sorting</remarks>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="nodes">All nodes of directed acyclic graph.</param>
+        /// <param name="edges">All edges of directed acyclic graph.</param>
+        /// <returns>Sorted nodes in topological order.</returns>
+        public static List<T> TopologicalSort<T>(HashSet<T> nodes, HashSet<(T, T)> edges) where T : IEquatable<T>
+        {
+            // Empty list that will contain the sorted elements
+            var L = new List<T>();
+
+            // Set of all nodes with no incoming edges
+            var S = new HashSet<T>(nodes.Where(n => edges.All(e => e.Item2.Equals(n) == false)));
+
+            // while S is non-empty do
+            while (S.Any())
+            {
+
+                //  remove a node n from S
+                var n = S.First();
+                S.Remove(n);
+
+                // add n to tail of L
+                L.Add(n);
+
+                // for each node m with an edge e from n to m do
+                foreach (var e in edges.Where(e => e.Item1.Equals(n)).ToList())
+                {
+                    var m = e.Item2;
+
+                    // remove edge e from the graph
+                    edges.Remove(e);
+
+                    // if m has no other incoming edges then
+                    if (edges.All(me => me.Item2.Equals(m) == false))
+                    {
+                        // insert m into S
+                        S.Add(m);
+                    }
+                }
+            }
+
+            // if graph has edges then
+            if (edges.Any())
+            {
+                // return error (graph has at least one cycle)
+                return null;
+            }
+            else
+            {
+                // return L (a topologically sorted order)
+                return L;
+            }
+        }
     }
 }
---
+++ AdventOfCode2023Tests/Utils/ExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode2023.Utils;
+using AdventOfCode2023.Utils.Graph;
+using NUnit.Framework;
+
+namespace AdventOfCode2023Tests.Utils
+{
+    public class ExtensionsTests
+    {
+        [Test()]
+        public void TopologicalSortTest()
+        {
+            var nodes = new HashSet<int>(new[] { 7, 5, 3, 8, 11, 2, 9, 10 });
+            var edges = new HashSet<(int, int)>(
+                new[] {
+                    (7, 11),
+                    (7, 8),
+                    (5, 11),
+                    (3, 8),
+                    (3, 10),
+                    (11, 2),
+                    (11, 9),
+                    (11, 10),
+                    (8, 9)});
+
+            var ret = ExtensionMethods.TopologicalSort(
+                nodes,
+                edges
+            );
+
+            Assert.That(ret, Is.EqualTo(new[] { 7, 5, 11, 2, 3, 10, 8, 9 }));
+        }
+
+        [Test()]
+        public void TopologicalSortOnGraphTest()
+        {
+            var graph = new WeightedGraph();
+            graph.AddConnection(new("7"), new("11"), 1);
+            graph.AddConnection(new("7"), new("8"), 1);
+            graph.AddConnection(new("5"), new("11"), 1);
+            graph.AddConnection(new("3"), new("8"), 1);
+            graph.AddConnection(new("3"), new("10"), 1);
+            graph.AddConnection(new("11"), new("2"), 1);
+            graph.AddConnection(new("11"), new("9"), 1);
+            graph.AddConnection(new("11"), new("10"), 1);
+            graph.AddConnection(new("8"), new("9"), 1);
+
+            var ret = graph.TopologicalSort();
+
+            Assert.That(ret.Select(n => n.Name), Is.EqualTo(new[] { "7", "5", "11", "2", "3", "10", "8", "9" }));
+        }
+    }
+}
